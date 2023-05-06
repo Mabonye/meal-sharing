a96666719251a12c9fb0cf12cabccef704481954
Mima <mimmamenser@yahoo.com>
2023-05-07 00:09:40 +0200

    Meal sharing files were modified

--- src/client/components/MealsList/MealsList.jsx
+++ src/client/components/MealsList/MealsList.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
-import MealItem from './MealItem/MealItem';
+import React, { useState, useEffect } from 'react';
+import "./MealsList.css";
 
 function MealsList() {
+    const [meals, setMeals] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3005/all-meals')
+            .then(response => response.json())
+            .then(data => setMeals(data))
+            .catch(error => console.error(error));
+    }, [meals]);
+
     return (
         <div>
             <h1>Our Menu</h1>
-            <MealItem />
+            <div>
+                {meals.map(meal => {
+                    return (
+                        <div key={meal.id} className='meal'>
+                            <div className='meal-description'>
+                                <h2>{meal.title}</h2>
+                                <p>{meal.description}</p>
+                            </div>
+                            <div className='meal-price'>
+                                ${meal.price}
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     );
 }
